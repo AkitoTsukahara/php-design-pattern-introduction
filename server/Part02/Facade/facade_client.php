1abb9915a1ee657c70a44c0785f35e0565145342
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-05-01 23:14:11 +0900

    lesson Facade

---
+++ server/Part02/Facade/facade_client.php
@@ -0,0 +1,13 @@
+<?php
+
+$order = new Order();
+$item_dao = ItemDao::getInstance();
+
+$order->addItem(new OrderItem($item_dao->findById(1), 2));
+$order->addItem(new OrderItem($item_dao->findById(2), 1));
+$order->addItem(new OrderItem($item_dao->findById(3), 3));
+
+/**
+ * 注文処理はこの1行だけ
+ */
+OrderManager::order($order);
